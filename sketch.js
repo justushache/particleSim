3b6d48b1e0e3c894fd8520d5a763ce50fefb414c
unknown <justushache@yahoo.com>
2020-10-18 15:09:30 +0200

    initial upload

---
+++ sketch.js
@@ -0,0 +1,114 @@
+// velocity = mm/s
+// pos = mm from origin
+// size = mm
+let SPEED = 1000 // calculations per second in the model
+let particles
+let walls
+function setup() {
+  createCanvas(400, 400);
+  // create array of particles
+  // create array of walls
+  walls = createWalls()
+  particles = [new Particle(createVector(5,200),10,createVector(150,50))]
+}
+function draw() {
+  background(220);
+  //update all particles
+  particles.forEach(part => part.update(particles, walls))
+  //draw all walls
+  walls.forEach(wall => wall.draw())
+  //draw all particles
+  particles.forEach(part => part.draw())
+  frameRate(SPEED)
+  if(frameCount%1 == 0){
+    particles.push(new Particle(createVector(5,200+random(10)),2,createVector(30,random(-5,-2))))
+  }
+}
+
+function createWalls(){
+  let walls = []
+  walls.push(new Wall(createVector(0,220),createVector(60,200),5))
+  valve(0,1.5,walls)
+  valve(160,1.5,walls)
+  valve(320,1.5,walls)
+  valve(80,-1.5,walls)
+  valve(240,-1.5,walls)
+  valve(400,-1.5,walls)
+  return walls
+}
+
+function valve(x,dir,walls){
+  walls.push(new Wall(createVector(x+0,200-0*dir),createVector(x+30,200-10*dir),5))
+  walls.push(new Wall(createVector(x+30,200-10*dir),createVector(x+100,200-50*dir),5))
+  walls.push(new Wall(createVector(x+100,200-50*dir),createVector(x+120,200-50*dir),5))
+  walls.push(new Wall(createVector(x+120,200-50*dir),createVector(x+140,200-30*dir),5))
+  walls.push(new Wall(createVector(x+140,200-30*dir),createVector(x+130,200-0*dir),5))
+  walls.push(new Wall(createVector(x+130,200-0*dir),createVector(x+160,200-0*dir),5))
+  
+  walls.push(new Wall(createVector(x+60,200-10*dir),createVector(x+100,200-30*dir),5))
+  walls.push(new Wall(createVector(x+100,200-30*dir),createVector(x+100,200-10*dir),5))
+  walls.push(new Wall(createVector(x+100,200-10*dir),createVector(x+60,200-10*dir),5))
+  return walls
+}
+
+class Wall{
+  constructor(pt1,pt2,thickness){
+    this.pt1 = pt1
+    this.pt2 = pt2
+    this.n = pt1.copy().sub(pt2)
+    this.thickness = thickness
+  }
+  //function to draw a wall
+  draw(){
+      strokeWeight(this.thickness)
+      line(this.pt1.x,this.pt1.y,this.pt2.x,this.pt2.y)
+    }
+}
+
+class Particle{
+  constructor(pos, size, velocity){
+    this.pos = pos
+    this.size = size
+    this.velocity = velocity
+  }
+  draw(){
+    circle(this.pos.x, this.pos.y, this.size)
+  }
+  
+  update(particles, walls){
+    //calculate forces between particles and new velocity
+    for(let i = 0; i < particles.length; i++){
+      let particle = particles[i]
+      let force = this.pos.copy().sub(particle.pos)
+      if(force.mag()>20)
+      this.velocity.add(force.mult(1/(force.mag())))
+    }
+    
+    //reflect particles off walls and calculate new velocity
+    for(let i = 0; i < walls.length; i++){
+      let wall = walls[i]
+      if(this.reflectOfWall(wall)){
+        break
+      }
+    }
+    //calculate new position of particle
+    this.pos.x += this.velocity.x/SPEED
+    this.pos.y += this.velocity.y/SPEED
+  }
+  reflectOfWall(wall){
+    let a = wall.pt1.copy().sub(wall.pt2)
+    let b = wall.pt2.copy().sub(this.pos)
+    let c = this.pos.copy().sub(this.velocity.copy().div(SPEED))
+    
+    //a particle should be reflected, if its path intersects with a line
+    let facA = b.y*c.x-b.x*c.y/(a.y*b.x-a.x*b.y)
+    let facB = c.y+facA*a.y/b.y
+    
+    if(facA<1){
+      console.log("inside")
+      this.velocity = this.velocity.rotate(this.velocity.angleBetween(wall.n)*2)
+      return true;
+    }
+    return false;
+  }
+}
